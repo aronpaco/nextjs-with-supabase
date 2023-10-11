55c9933beb3a67118889e3bd34f6feb822d8365b
aron <114922035+aronpaco@users.noreply.github.com>
2023-10-11 15:57:34 +0300

    route.jsx post fix

--- app/api/todo/route.jsx
+++ app/api/todo/route.jsx
@@ -4,7 +4,7 @@ export async function GET() {
   return NextResponse.json('helo')
 }
 
-export async function POST() {
+export async function POST(req) {
     const data = await req.json()
     return NextResponse.json(data.hello)
   }
